d64b87cb92d227872e7cc63545b4c2834f851124
Kibet <102409337+KibetDennis@users.noreply.github.com>
2024-05-14 15:14:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo'hello world';
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
